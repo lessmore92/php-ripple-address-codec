d8222f1f7c7213349eade74d9340455f196e6963
Roman Mátyus <romanmatyus@gmail.com>
2021-06-21 20:47:05 +0200

    Update for new PHP

--- src/RippleAddressCodec.php
+++ src/RippleAddressCodec.php
@@ -115,7 +115,7 @@ class RippleAddressCodec
 
     public function encodeXAddress(Buffer $accountId, $tag, bool $test = false): string
     {
-        $flag = $tag === false ? 0 : $tag <= MAX_32_BIT_UNSIGNED_INT ? 1 : 2;
+        $flag = ($tag === false ? 0 : $tag <= MAX_32_BIT_UNSIGNED_INT) ? 1 : 2;
         if ($flag === 2)
         {
             throw new Exception('Invalid tag');
